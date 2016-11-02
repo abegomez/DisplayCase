cfbe53cdb0b414176a0f2efd7f7c5ed2dc078b01
Abraham Gomez <agomez717@gmail.com>
2016-11-01 23:12:48 -0700

    Create outline of a layout in a scrollview layout

--- app/src/main/java/myapp/abrahamjohngomez/com/displaycase/SingleItemDisplayActivity.java
+++ app/src/main/java/myapp/abrahamjohngomez/com/displaycase/SingleItemDisplayActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class SingleItemDisplayActivity extends FragmentActivity {
     CollectionPagerAdapter mCollectionPagerAdapter;
@@ -14,11 +15,15 @@ public class SingleItemDisplayActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_item_display);
 
+        //layout a = (layout) findviewbyid(the linear layout)
+        LinearLayout b = new LinearLayout(this);
+        b.setOrientation(LinearLayout.HORIZONTAL);
+        //so on and so forth
+
         mCollectionPagerAdapter = new CollectionPagerAdapter(
                 getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pagersingleitem);
         mViewPager.setAdapter(mCollectionPagerAdapter);
-
     }
 }
 
